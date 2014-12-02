6f1f13f795f09ebc74b2e1468bb1fc2fb0a4a0ee
ChienliMa <maqianlie@gmail.com>
2014-12-02 22:09:39 +0800

    change layout

--- lib/ppc/api/baidu/report.rb
+++ lib/ppc/api/baidu/report.rb
@@ -7,11 +7,11 @@ module PPC
       
         # 需要用到的映射集合
         Type_map = { 'account' => 2, 'plan'=> 10, 'group'=> 11, 
-                                              'keyword'=> 14, 'creative'=> 12, 'pair'=> 15, 
-                                              'region'=> 3, 'wordid'=> 9 }
+                      'keyword'=> 14, 'creative'=> 12, 'pair'=> 15, 
+                       'region'=> 3, 'wordid'=> 9 }
         Level_map = {  'account' => 2, 'plan' => 3, 'group' => 5, 
-                                  'creative' => 7, 'keywordid' => 11, 'pair' => 12, 
-                                  'wordid' => 6 }
+                        'creative' => 7, 'keywordid' => 11, 'pair' => 12, 
+                         'wordid' => 6 }
         Device_map = { 'all' => 0, 'pc' => 1, 'mobile' => 2 }
         Unit_map = { 'day' => 5, 'week' => 4, 'month' => 3, 'year' => 1, 'hour' => 7 }
 
@@ -80,15 +80,15 @@ module PPC
             requesttype = {}
             startDate, endDate = get_date( param )
 
-            requesttype[:performanceData]   =     param[:fields]        ||     %w(click impression)
-            requesttype[:reportType]               =     Type_map[ param[:type] ]          if  param[:type] 
-            requesttype[:levelOfDetails]          =     Level_map[  param[:level] ]        if param[:level]
-            requesttype[:statRange]                 =     Level_map[ param[:range] ]       if param[:range]
-            requesttype[:unitOfTime]               =     Unit_map[ param[:unit] ]           if param[:unit] 
-            requesttype[:number]                     =    param[:number]                         if  param[:number]
-            requesttype[:device]                        =    Device_map[ param[:device] ]  if param[:device]
-            requesttype[:startDate]                  =     startDate
-            requesttype[:endDate]                    =     endDate
+            requesttype[:performanceData]   =     param[:fields]   ||   %w(click impression)
+            requesttype[:reportType]        =     Type_map[ param[:type] ]      if  param[:type] 
+            requesttype[:levelOfDetails]    =     Level_map[  param[:level] ]   if param[:level]
+            requesttype[:statRange]         =     Level_map[ param[:range] ]    if param[:range]
+            requesttype[:unitOfTime]        =     Unit_map[ param[:unit] ]      if param[:unit] 
+            requesttype[:number]            =     param[:number]                if  param[:number]
+            requesttype[:device]            =     Device_map[ param[:device] ]  if param[:device]
+            requesttype[:startDate]         =     startDate
+            requesttype[:endDate]           =     endDate
             requesttypes << requesttype
           end
           return requesttypes
@@ -105,15 +105,15 @@ module PPC
             requesttype = {}
             startDate, endDate = get_date( param )
 
-            requesttype[:performanceData]   =     param[:fields]        ||     %w(click impression)
-            requesttype[:reportType]               =     Type_map[ param[:type] ]          if  param[:type]
-            requesttype[:levelOfDetails]          =     Level_map[  param[:level] ]        if param[:level]
-            requesttype[:statRange]                 =     Level_map[ param[:range] ]       if param[:range]
-            requesttype[:unitOfTime]               =     Unit_map[ param[:unit] ]           if param[:unit]
-            requesttype[:device]                        =    Device_map[ param[:device] ]  if param[:device]
-            requesttype[:idOnly]                        =    param[:id_only]      ||    false
-            requesttype[:startDate]                   =    startDate
-            requesttype[:endDate]                     =    endDate
+            requesttype[:performanceData]   =     param[:fields]  ||  %w(click impression)
+            requesttype[:reportType]        =     Type_map[ param[:type] ]     if  param[:type]
+            requesttype[:levelOfDetails]    =     Level_map[  param[:level] ]  if param[:level]
+            requesttype[:statRange]         =     Level_map[ param[:range] ]   if param[:range]
+            requesttype[:unitOfTime]        =     Unit_map[ param[:unit] ]     if param[:unit]
+            requesttype[:device]            =    Device_map[ param[:device] ]  if param[:device]
+            requesttype[:idOnly]            =    param[:id_only]  ||  false
+            requesttype[:startDate]         =    startDate
+            requesttype[:endDate]           =    endDate
             requesttypes << requesttype
           end
           return requesttypes
